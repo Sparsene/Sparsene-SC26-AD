aa8f2895aae3544d9fc471bdaf8c9893bb9b4032
Sparsene <277935663+Sparsene@users.noreply.github.com>
2026-07-23 02:22:13 +0000

    update

--- generate_figures_tables.sh
+++ generate_figures_tables.sh
@@ -23,7 +23,7 @@ python process_dtc_test_csv.py ../results/DTCSpMM_exe_time_and_throughput.csv ..
 # python process_cusparse_test_log.py ../results/cusparse_fp32_N256.log ../results/jsons/cusparse_fp32_N256.json fp32
 python process_cusparse_test_log.py ../results/cusparse_fp32_N512.log ../results/jsons/cusparse_fp32_N512.json fp32
 # sparsetir
-python process_sparsetir_test_log.py ../SparseTIR-exp/examples/spmm/sparsetir.log ../results/jsons/
+# python process_sparsetir_test_log.py ../SparseTIR-exp/examples/spmm/sparsetir.log ../results/jsons/
 # sputnik
 # python process_sputnik_test_csv.py ../FlashSparse/result/Baseline/spmm/rode_spmm_f32_n128.csv ../results/jsons/sputnik_fp32_N128.json 128
 # python process_sputnik_test_csv.py ../FlashSparse/result/Baseline/spmm/rode_spmm_f32_n256.csv ../results/jsons/sputnik_fp32_N256.json 256
@@ -52,12 +52,13 @@ popd
 #! T4: pipeline simulator
 pushd $SPARSENE_AD_ROOT/sparsene/simulator
 # generate table IV
-python eval_topk_precision.py --json ./results/a100/acc/combined_results.json
-python eval_topk_precision.py --json ./results/a100/bitbsr/combined_results.json
+python eval_topk_precision.py --json ./results/a100_sampled/acc/combined_results.json
+python eval_topk_precision.py --json ./results/a100_sampled/bitbsr/combined_results.json
 popd
 
 #! T5: search convergence
-pushd $SPARSENE_AD_ROOT/sparsene/search_convergence
+pushd $SPARSENE_AD_ROOT/sparsene/examples/src_fp32/acc/testbed/scripts/motivation_scripts/sc_search_6156plans
+# /workspace/Sparsene-SC26-AD/sparsene/examples/src_fp32/acc/testbed/scripts/motivation_scripts/sc_search_6156plans
 # plot fig14
 python plot_sim_vs_nosim_curve.py \
   --sim-json results/run_sim_cluster_hybrid.json \
